735db58b9f248f2b2bcc77c1d5cd63f241f22961
Volodymyr Hladkyi <vova.gladkiii@gmail.com>
2022-06-20 14:32:55 +0300

    MIDDLEWARES | authToken

--- src/controllers/interfaces.ts
+++ src/controllers/interfaces.ts
@@ -1,6 +1,13 @@
+import { Request } from "express";
+
+import { EUserType } from "../config/enums";
 
 
 export interface IBaseRequest {
-    type: string;
-    profileId: string;
+    type?: keyof typeof EUserType;
+    id: string;
+}
+
+export interface IReqWithToken extends Request {
+    user: IBaseRequest;
 }
--- src/middlewares/auth/index.ts
+++ src/middlewares/auth/index.ts
@@ -1,1 +1,31 @@
+import { Request, Response, NextFunction } from 'express';
+
 import db from '../../db/all-models';
+import getToken from '../../helpers/tokenHelpers/get-token';
+import verifyToken from '../../helpers/tokenHelpers/verify-token';
+import ApiError from '../../errors/api-error';
+import { IReqWithToken } from '../../controllers/interfaces';
+
+
+export default { 
+    async authToken(
+        req: IReqWithToken,
+        res: Response,
+        next: NextFunction
+    ): Promise<void> {
+        try {
+            if (!req.headers.authorization) throw ApiError.unauthorized('No auth headers');
+
+            const accessToken = getToken(req);
+            if (!accessToken) throw ApiError.unauthorized('No token');
+
+            const verifiedData = verifyToken(accessToken, 'user_access_secret');
+            if (!verifiedData) throw ApiError.unauthorized('Invalid access token');
+
+            req.user = verifiedData;
+            next();
+        } catch (err) {
+            next(ApiError.unauthorized(err.message, [err]));
+        }
+    }
+}
