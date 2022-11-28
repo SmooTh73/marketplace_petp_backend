87d38331c5e9c71e4f2f55a9ecea430797f33eb8
Volodymyr Hladkyi <vova.gladkiii@gmail.com>
2022-11-28 09:33:28 +0200

    USER | save changes

--- src/routes/user-router.ts
+++ src/routes/user-router.ts
@@ -2,14 +2,12 @@ import { Router } from 'express';
 import userController from '../controllers/user.controller';
 import validationMiddleware from '../middlewares/validation/index';
 import userSchema from '../validation/user/user-object-schema';
-import userTypeSchema from '../validation/user/user-type-schema';
-
 
 const userRouter = Router();
 
 //________________POST_______________//
 userRouter.post(
-    '/register/:type',
+    '/register',
     validationMiddleware(userSchema, 'body'),
     userController.register
 );
--- src/validation/user/user-type-schema.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-import { EUserType } from '../../config/enums';
-
-
-export default yup.object({
-    type: yup.string().oneOf(Object.keys(EUserType)).required()
-})
