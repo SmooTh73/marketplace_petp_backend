d714736702c8a74ad06a851cb1b0be0d5f4973d5
Volodymyr Hladkyi <vova.gladkiii@gmail.com>
2022-12-01 16:50:30 +0200

    USER | profile dto

---
+++ src/services/userService/dto/profile.dto.ts
@@ -0,0 +1,18 @@
+import User from 'src/db/models/user.model';
+
+
+export class ProfileDto {
+    id: string;
+    name: string;
+    surname: string;
+    email: string;
+    role: string;
+
+    constructor(data: User) {
+        this.id = data.id;
+        this.name = data.name;
+        this.surname = data.surname;
+        this.email = data.email;
+        this.role = data.role;
+    }
+}
