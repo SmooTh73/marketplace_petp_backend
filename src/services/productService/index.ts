49b558d55f1f286775d4923a8cc33df1f17f313f
SmooTh73 <vova.gladkiii@gmail.com>
2022-12-05 15:54:39 +0200

    PRODUCT | create upd

--- src/services/productService/index.ts
+++ src/services/productService/index.ts
@@ -12,6 +12,13 @@ export default {
         const store = await db.Store.findOne({ where: { userId }, attributes: ['id']});
         if (!store) throw ApiError.badRequest('User doesn\'t have store');
 
+        const match = await db.CategoryBrand.findOne(
+            { 
+                where: { brandId: attrs.brandId, categoryId: attrs.categoryId }
+            }
+        );
+        if (!match) throw ApiError.badRequest('Category and brand don\'t match.');
+        
         return await db.Product.create({ ...attrs, storeId: store.id });
     }
 }
