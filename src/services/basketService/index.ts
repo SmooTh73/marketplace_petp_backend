29ba7071a8bc925c01fb475bcdf07fcaf4c5bd6b
SmooTh73 <vova.gladkiii@gmail.com>
2022-12-14 12:17:51 +0200

    BASKET | services

---
+++ src/services/basketService/index.ts
@@ -0,0 +1,33 @@
+import Basket from '../../db/models/basket.model';
+import db from '../../db/all-models';
+import BasketProduct from 'src/db/models/basket-product.model';
+
+
+export default {
+    async create(userId: string): Promise<Basket> {
+        return await db.Basket.create({ userId });
+    },
+
+    async addProduct(
+        productId: string,
+        userId: string
+    ): Promise<BasketProduct> {
+        const basket = await db.Basket.findOne({ where: { userId }, attributes: ['id'] });
+        return await db.BasketProduct.create(
+            { productId, basketId: basket.id },
+            { 
+                include: [
+                    { model: db.Product, as: 'product', attributes: ['id', 'title', 'image'] }
+                ]
+            }
+        );
+    },
+
+    async deleteProduct(
+        productId: string,
+        userId: string
+    ): Promise<void> {
+        const basket = await db.Basket.findOne({ where: { userId }, attributes: ['id'] });
+        await db.BasketProduct.destroy({ where: { productId, basketId: basket.id }});
+    }
+}
