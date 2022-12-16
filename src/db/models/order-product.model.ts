d70f981f7865967aa70f7d3a7fee6b6d2c2c5bd0
SmooTh73 <vova.gladkiii@gmail.com>
2022-12-16 18:57:37 +0200

    ORDER_PRODUCT | fix model

--- src/db/models/order-product.model.ts
+++ src/db/models/order-product.model.ts
@@ -1,5 +1,6 @@
-import { BelongsTo, Column, DataType, ForeignKey, Model, Table } from 'sequelize-typescript';
+import { BelongsTo, Column, DataType, ForeignKey, Index, Model, Table } from 'sequelize-typescript';
 import Order from './order.model';
+import Product from './product.model';
 
 
 @Table({ tableName: 'order_product' })
@@ -7,10 +8,25 @@ export default class OrderProduct extends Model<OrderProduct> {
     @Column({ type: DataType.UUID, defaultValue: DataType.UUIDV4, primaryKey: true, unique: true })
     id: string;
 
+    @Column({ type: DataType.INTEGER, allowNull: false, validate: { min: 1 } })
+    amount: number;
+
+    @Index({
+        name: 'unique_order_product',
+        unique: true
+    })
     @ForeignKey(() => Order)
     @Column({ type: DataType.UUID })
     orderId: string;
 
     @BelongsTo(() => Order)
     order: Order;
+
+    @Index('unique_order_product')
+    @ForeignKey(() => Product)
+    @Column({ type: DataType.UUID })
+    productId: string;
+
+    @BelongsTo(() => Product)
+    product: Product;
 }
